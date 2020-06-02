5f383f300c06536c4214d56c7a6abb16faad74ad
bex <t19319@kist.re.kr>
2020-06-02 14:51:58 +0900

    resnet and fpn graph

---
+++ model/model.py
@@ -0,0 +1,190 @@
+import torch.nn as nn
+import torch.functional as F
+import torch, torchvision
+
+
+############################################################
+#  Resnet Graph
+############################################################
+
+class SamePad2d(nn.Module):
+    """Mimics tensorflow's 'SAME' padding.
+    """
+
+    def __init__(self, kernel_size, stride):
+        super(SamePad2d, self).__init__()
+        self.kernel_size = torch.nn.modules.utils._pair(kernel_size)
+        self.stride = torch.nn.modules.utils._pair(stride)
+
+    def forward(self, input):
+        in_width = input.size()[2]
+        in_height = input.size()[3]
+        out_width = math.ceil(float(in_width) / float(self.stride[0]))
+        out_height = math.ceil(float(in_height) / float(self.stride[1]))
+        pad_along_width = ((out_width - 1) * self.stride[0] +
+                           self.kernel_size[0] - in_width)
+        pad_along_height = ((out_height - 1) * self.stride[1] +
+                            self.kernel_size[1] - in_height)
+        pad_left = math.floor(pad_along_width / 2)
+        pad_top = math.floor(pad_along_height / 2)
+        pad_right = pad_along_width - pad_left
+        pad_bottom = pad_along_height - pad_top
+        return F.pad(input, (pad_left, pad_right, pad_top, pad_bottom), 'constant', 0)
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride)
+        self.bn1 = nn.BatchNorm2d(planes, eps=0.001, momentum=0.01)
+        self.padding2 = SamePad2d(kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3)
+        self.bn2 = nn.BatchNorm2d(planes, eps=0.001, momentum=0.01)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1)
+        self.bn3 = nn.BatchNorm2d(planes * 4, eps=0.001, momentum=0.01)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.padding2(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(self, architecture, stage5=False):
+        super(ResNet, self).__init__()
+        assert architecture in ["resnet50", "resnet101"]
+        self.inplanes = 64
+        self.layers = [3, 4, {"resnet50": 6, "resnet101": 23}[architecture], 3]
+        self.block = Bottleneck
+        self.stage5 = stage5
+
+        self.C1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64, eps=0.001, momentum=0.01),
+            nn.ReLU(inplace=True),
+            SamePad2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.C2 = self.make_layer(self.block, 64, self.layers[0])
+        self.C3 = self.make_layer(self.block, 128, self.layers[1], stride=2)
+        self.C4 = self.make_layer(self.block, 256, self.layers[2], stride=2)
+        if self.stage5:
+            self.C5 = self.make_layer(self.block, 512, self.layers[3], stride=2)
+        else:
+            self.C5 = None
+
+    def forward(self, x):
+        x = self.C1(x)
+        x = self.C2(x)
+        x = self.C3(x)
+        x = self.C4(x)
+        x = self.C5(x)
+        return x
+
+
+    def stages(self):
+        return [self.C1, self.C2, self.C3, self.C4, self.C5]
+
+    def make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride),
+                nn.BatchNorm2d(planes * block.expansion, eps=0.001, momentum=0.01),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+
+############################################################
+#  FPN Graph
+############################################################
+
+class FPN(nn.Module):
+    def __init__(self, C1, C2, C3, C4, C5, out_channels):
+        super(FPN, self).__init__()
+        self.out_channels = out_channels
+        self.C1 = C1
+        self.C2 = C2
+        self.C3 = C3
+        self.C4 = C4
+        self.C5 = C5
+        self.P6 = nn.MaxPool2d(kernel_size=1, stride=2)
+        self.P5_conv1 = nn.Conv2d(2048, self.out_channels, kernel_size=1, stride=1)
+        self.P5_conv2 = nn.Sequential(
+            SamePad2d(kernel_size=3, stride=1),
+            nn.Conv2d(self.out_channels, self.out_channels, kernel_size=3, stride=1),
+        )
+        self.P4_conv1 =  nn.Conv2d(1024, self.out_channels, kernel_size=1, stride=1)
+        self.P4_conv2 = nn.Sequential(
+            SamePad2d(kernel_size=3, stride=1),
+            nn.Conv2d(self.out_channels, self.out_channels, kernel_size=3, stride=1),
+        )
+        self.P3_conv1 = nn.Conv2d(512, self.out_channels, kernel_size=1, stride=1)
+        self.P3_conv2 = nn.Sequential(
+            SamePad2d(kernel_size=3, stride=1),
+            nn.Conv2d(self.out_channels, self.out_channels, kernel_size=3, stride=1),
+        )
+        self.P2_conv1 = nn.Conv2d(256, self.out_channels, kernel_size=1, stride=1)
+        self.P2_conv2 = nn.Sequential(
+            SamePad2d(kernel_size=3, stride=1),
+            nn.Conv2d(self.out_channels, self.out_channels, kernel_size=3, stride=1),
+        )
+
+    def forward(self, x):
+        x = self.C1(x)
+        x = self.C2(x)
+        c2_out = x
+        x = self.C3(x)
+        c3_out = x
+        x = self.C4(x)
+        c4_out = x
+        x = self.C5(x)
+        c5_out = x
+        p5_out = self.P5_conv1(x)
+        p4_out = self.P4_conv1(c4_out) + F.upsample(p5_out, scale_factor=2)
+        p3_out = self.P3_conv1(c3_out) + F.upsample(p4_out, scale_factor=2)
+        p2_out = self.P2_conv1(c2_out) + F.upsample(p3_out, scale_factor=2)
+
+        p5_out = self.P5_conv2(p5_out)
+        p4_out = self.P4_conv2(p4_out)
+        p3_out = self.P3_conv2(p3_out)
+        p2_out = self.P2_conv2(p2_out)
+
+        # P6 is used for the 5th anchor scale in RPN. Generated by
+        # subsampling from P5 with stride of 2.
+        p6_out = self.P6(p5_out)
+
+        return [p2_out, p3_out, p4_out, p5_out, p6_out]
